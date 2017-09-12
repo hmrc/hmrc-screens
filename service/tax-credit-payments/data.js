5f63f59eac97d0a5d2baf6bd41b81b141c2d7f77
ladine.cook@digital.hmrc.gov.uk <ladine.cook@digital.hmrc.gov.uk>
2017-09-12 15:20:29 +0100

    tax credit payment scenarios

--- service/tax-credit-payments/data.js
+++ service/tax-credit-payments/data.js
@@ -16,6 +16,155 @@ var data = {
           "note": "Notes go here..."
         }
       ]
+    },
+    {
+      "title": "wtc weekly",
+      "path": [
+        {
+          "caption": "tax credit summary",
+          "imgref": "images/wtc-weekly/01-tax-credit-summary.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "payments",
+          "imgref": "images/wtc-weekly/02-payments.png",
+          "note": "Notes go here..."
+        }
+      ]
+    },
+    {
+      "title": "ctc 4 weekly",
+      "path": []
+    },
+    {
+      "title": "wtc ctc weekly",
+      "path": [
+        {
+          "caption": "tax credit summary",
+          "imgref": "images/wtc-ctc-weekly/01-tax-credit-summary.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "payments",
+          "imgref": "images/wtc-ctc-weekly/02-payments.png",
+          "note": "Notes go here..."
+        }
+      ]
+    },
+    {
+      "title": "wtc ctc 4 weekly",
+      "path": [
+        {
+          "caption": "tax credit summary",
+          "imgref": "images/wtc-ctc-4-weekly/01-tax-credit-summary.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "payments",
+          "imgref": "images/wtc-ctc-4-weekly/02-payments.png",
+          "note": "Notes go here..."
+        }
+      ]
+    },
+    {
+      "title": "wtc weekly ctc 4 weekly",
+      "path": [
+        {
+          "caption": "tax credit summary",
+          "imgref": "images/wtc-weekly-ctc-4-weekly/01-tax-credit-summary.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "payments",
+          "imgref": "images/wtc-weekly-ctc-4-weekly/02-payments.png",
+          "note": "Notes go here..."
+        }
+      ]
+    },
+    {
+      "title": "multiple payments same day",
+      "path": [
+        {
+          "caption": "tax credit summary",
+          "imgref": "images/multiple-payments-same-day/01-tax-credit-summary.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "payments",
+          "imgref": "images/multiple-payments-same-day/02-payments.png",
+          "note": "Notes go here..."
+        }
+      ]
+    },
+    {
+      "title": "wtc stopping",
+      "path": [
+        {
+          "caption": "tax credit summary",
+          "imgref": "images/wtc-stopping/01-tax-credit-summary.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "payments",
+          "imgref": "images/wtc-stopping/02-payments.png",
+          "note": "Notes go here..."
+        }
+      ]
+    },
+    {
+      "title": "ftnae with payments",
+      "path": [
+        {
+          "caption": "payments",
+          "imgref": "images/ftnae-with-payments/01-payments.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "tax credit summary",
+          "imgref": "images/ftnae-with-payments/02-tax-credit-summary.png",
+          "note": "Notes go here..."
+        }
+      ]
+    },
+    {
+      "title": "ftnae no payments pre september",
+      "path": [
+        {
+          "caption": "tax credit summary",
+          "imgref": "images/ftnae-no-payments-pre-september/01-tax-credit-summary.png",
+          "note": "Notes go here..."
+        }
+      ]
+    },
+    {
+      "title": "ftnae no payments post september",
+      "path": [
+        {
+          "caption": "tax credit summary",
+          "imgref": "images/ftnae-no-payments-post-september/01-tax-credit-summary.png",
+          "note": "Notes go here..."
+        }
+      ]
+    },
+    {
+      "title": "help us improve survey",
+      "path": [
+        {
+          "caption": "page useful",
+          "imgref": "images/help-us-improve-survey/01-page-useful.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "how can we improve",
+          "imgref": "images/help-us-improve-survey/02-how-can-we-improve.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "thank you",
+          "imgref": "images/help-us-improve-survey/03-thank-you.png",
+          "note": "Notes go here..."
+        }
+      ]
     }
   ]
 }
