39d394a0dd13cfc7ff5bcd0fb5c82728d69bc90f
ladine.cook@digital.hmrc.gov.uk <ladine.cook@digital.hmrc.gov.uk>
2017-09-12 11:49:05 +0100

    ctc weekly

---
+++ service/tax-credit-payments/data.js
@@ -0,0 +1,21 @@
+var data = {
+  "service": "Tax credit payments",
+  "last-updated": "12th September 2017",
+  "userjourneys": [
+    {
+      "title": "ctc weekly",
+      "path": [
+        {
+          "caption": "tax credit summary",
+          "imgref": "images/ctc-weekly/01-tax-credit-summary.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "payments",
+          "imgref": "images/ctc-weekly/02-payments.png",
+          "note": "Notes go here..."
+        }
+      ]
+    }
+  ]
+}
